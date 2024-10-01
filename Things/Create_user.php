5b437620dd009f84f2ba598f4343b43bf7669f17
Ghost0mega <sguyenot009@gmail.com>
2024-10-01 14:22:41 +0200

    Update Create_user.php

--- Things/Create_user.php
+++ Things/Create_user.php
@@ -1,14 +1,46 @@
 <!DOCTYPE html>
 <html lang="fr">
+    <script>
+        function return_to_main() {
+            // Redirige vers le script PHP
+            window.location.href = 'main.php'; 
+        }
+        </script>
 <head>
     <meta charset="UTF-8">
     <title>Mon formulaire</title>
+    <style>
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+            text-align: center;
+            text-decoration: none;
+            outline: none;
+            color: #fff;
+            background-color: #4CAF50;
+            border: none;
+            border-radius: 5px;
+            box-shadow: 0 4px #999;
+        }
+
+        .button:hover {background-color: #3e8e41}
+
+        .button:active {
+            background-color: #3e8e41;
+            box-shadow: 0 2px #666;
+            transform: translateY(2px);
+        }
+    </style>
 </head>
 <body>
 
-<form action="Create_user.php" method="post"> <!-- Changez 'traitement.php' pour votre script PHP -->
+<form action="Create_user.php" method="post">
     <input type="submit" value="Envoyer" />
 </form>
+
+<a class="button" onclick="return_to_main()">Retour au menu principal</a>
 
 </body>
 </html>

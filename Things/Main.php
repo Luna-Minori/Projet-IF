f3e06385895067b5f3a12357d91cb2099319f770
Ghost0mega <sguyenot009@gmail.com>
2024-10-15 14:17:41 +0200

    e

--- Things/Main.php
+++ Things/Main.php
@@ -3,7 +3,7 @@ session_start();
 
 // Vérifier si l'utilisateur est connecté
 if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
+    header('Location: login_user.php');
     exit();
 }
 ?>

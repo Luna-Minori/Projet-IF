998e18ec3d8e5ae302dca08ffb5b34747de1791f
Ghost0mega <sguyenot009@gmail.com>
2024-10-28 13:07:49 +0100

    le tableau marche paaaaas

--- Things/Main.php
+++ Things/Main.php
@@ -29,42 +29,66 @@ session_start();
                 <li class="logo_container">
                     <img class="logo" src="Image/logo.png">
                         </li>
-                            <li class="deroulant_Main"><a href="#"> Profile &ensp;</a>
+                            <li class="deroulant_Main"><a href="#"> Players &ensp;</a>
                                 <ul class="deroulant_Second">
                                    <li><a href="Login_user.php"> My Profile </a></li>
-                                   <li><a href="Create_user.php"> Account creation </a></li>
-                                   <li><a href="Create_user.php"> Tournament creation </a></li>
+                                   <li><a href="Create_user.php"> Browse Players </a></li>
                                 </ul>
                            </li>
-                        <li class="deroulant_Main"><a href="#"> Team &ensp;</a>
+                        <li class="deroulant_Main"><a href="#"> Teams &ensp;</a>
                             <ul class="deroulant_Second">
-                                <li><a> Account creation </a></li>
-                                <li><a> Team creation </a></li>
-                                <li><a> Tournament creation </a></li>
+                                <li><a> My Teams </a></li>
+                                <li><a> Join Teams </a></li>
+                                <li><a> Browse Teams </a></li>
                            </ul>
                         </li>
-                        <li class="deroulant_Main"><a href="#"> Tournament &ensp;</a>
+                        <li class="deroulant_Main"><a href="#"> Games &ensp;</a>
                                 <ul class="deroulant_Second">
-                                    <li><a> Account creation </a></li>
-                                    <li><a> Team creation </a></li>
-                                    <li><a> Tournament creation </a></li>    
+                                    <li><a> Add game </a></li>
+                                    <li><a> Browse games </a></li>
+                                </ul>
+                        <li class="deroulant_Main"><a href="#"> Tournaments &ensp;</a>
+                                <ul class="deroulant_Second">
+                                    <li><a> My tournaments </a></li>
+                                    <li><a> Join tournament </a></li>
+                                    <li><a> Browse tournaments </a></li>    
                                 </ul>
                             </li>
-                        <li class="deroulant_Main"><a href="#"> Account Creation &ensp;</a>
-                                <ul class="deroulant_Second">
-                                    <li><a> Account creation </a></li>
-                                    <li><a> Team creation </a></li>
-                                    <li><a> Tournament creation </a></li>
-                                </ul>
                          </li>
                     </ul>
                 </nav>
                 <div class = "content">
-                    <section id="upcoming-tournaments">
+                    <section id="welcome">
+                        <h2>WELCOME TO GAME ARENA</h2>
+                        <p>Manage your tournaments, teams, and players all in one place!</p>
+                    </section>
+                    <section id="tournaments-info">
                         <h2>Upcoming Tournaments</h2> 
-                        <p><strong>Name:</strong> Summer Championship 2024</p>
-                        <p><strong>Date:</strong> September 20, 2024</p>
-                        <p><strong>Location:</strong> Virtual Arena</p>
+                            <table id="upcoming-tournaments">
+                                <tr>
+                                    <th> Name </th>
+                                    <th> Game </th>
+                                    <th> Date </th>
+                                    <th> Location </th>
+                                </tr>
+                                <tr>
+                                    <td> Summer Championship 2024 </td>
+                                    <td> Chess </td>
+                                    <td> September 20, 2024 </td>
+                                    <td> Virtual Arena </td>
+                                </tr>
+                                <tr>
+                                    <td> Winter Championship 2024 </td>
+                                    <td> Uno </td>
+                                    <td> December 20, 2024 </td>
+                                    <td> Virtual Arena </td>
+                                </tr>
+                                <tr>
+                                    <td> Spring Championship 2025 </td>
+                                    <td> Poker </td>
+                                    <td> March 20, 2025 </td>
+                                    <td> Virtual Arena </td>
+                                </tr>
                     </section>
                 </div>
             </ul>

73e0d31a07740fb7f43d6951cc896d4fc5dcb533
Ghost0mega <sguyenot009@gmail.com>
2024-11-05 14:14:58 +0100

    undo

--- Things/Main.php
+++ Things/Main.php
@@ -1,15 +1,5 @@
 <?php
-<<<<<<< HEAD
     session_start();
-=======
-session_start();
-
-// Vérifier si l'utilisateur est connecté
-// if (!isset($_SESSION['username'])) {
-//     header('Location: login_user.php');
-//     exit();
-// }
->>>>>>> parent of 04c44fd (Merge branch 'main' into css-rebrand)
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +18,6 @@ session_start();
 </head>
 <body>
     <header>
-<<<<<<< HEAD
         <div class="img_container">
             <img class="fond" src="Image/fond.png">
                 <nav>
@@ -60,12 +49,7 @@ session_start();
                                 <li><a> Account creation </a></li>
                                 <li><a> Team creation </a></li>
                                 <li><a> Tournament creation </a></li>                                </ul>
-=======
-        <nav>
-            <ul>
-                <li class="logo_container">
-                    <img class="logo" src="Image/logo.png">
->>>>>>> parent of 04c44fd (Merge branch 'main' into css-rebrand)
+
                         </li>
                     <li class="deroulant_Main"><a href="#"> Account Creation &ensp;</a>
                             <ul class="deroulant_Second">
